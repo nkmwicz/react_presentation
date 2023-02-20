4bf56ab218003b27effd37b3d8e48388068569f5
Nathan Michalewicz <nathan@mwicz.com>
2023-02-20 16:10:47 -0500

    ADD types

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,7 +4,6 @@ import SplitSlide from './SplitSlide';
 import Arrows from './Arrows';
 import CenteredSlide from './CenteredSlide';
 import SplitSlideEmbed from './SplitSlideEmbed';
-import * as PropTypes from 'prop-types';
 
 /**
  * Layout is a default layout that uses takes a state array that follows the data model
@@ -13,70 +12,110 @@ import * as PropTypes from 'prop-types';
  * --ImagesToCache takes an array of images that should be
  * cached so they will be available before
  */
-function Layout({ 
-  slide, 
-  bgColor, 
-  color, 
-  nextClick, 
-  prevClick 
-}: {
-     slide: ,
-     bgColor: string,
-     colror: string,
-     nextClick:  
-    }) {
+interface LayoutTypes {
+  slide: {
+    title: string;
+    subTitle?: string;
+    byLine?: string;
+    intro?: boolean;
+    split?: boolean;
+    centered?: boolean;
+    splitEmbed?: boolean;
+    text?: Array<{
+      text: string;
+      color: string;
+      children?: Array<{ text: string; color: string }>;
+    }>;
+    image?: { image: string; description: string; alt: string };
+    images?: Array<{
+      image: string;
+      description: string;
+      flex: number;
+      alt: string;
+    }>;
+    embed?: { url: string; title: string };
+  };
+  bgColor?: string;
+  nextClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  prevClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  h1Class?: string | undefined;
+  headerClass?: string;
+  leftBoxClass?: string;
+  rightBoxClass?: string;
+  imgBoxClass?: string;
+  contentBoxClass?: string;
+  contentTextClass?: string;
+  imageArrayBoxClass?: string;
+  bgImage?: React.CSSProperties['backgroundImage'];
+}
+
+function Layout({
+  slide,
+  bgColor,
+  bgImage,
+  nextClick,
+  prevClick,
+  h1Class,
+  headerClass,
+  rightBoxClass,
+  imgBoxClass,
+  leftBoxClass,
+  contentBoxClass,
+  contentTextClass,
+  imageArrayBoxClass
+}: LayoutTypes): JSX.Element {
   return (
     <div className="main">
-      <Arrows handleNextClick={function (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
-        throw new Error('Function not implemented.');
-      } } handlePrevClick={function (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
-        throw new Error('Function not implemented.');
-      } } />
+      <Arrows handleNextClick={nextClick} handlePrevClick={prevClick} />
       {slide.intro && (
         <Intro
           title={slide.title}
           subTitle={slide.subTitle}
           byLine={slide.byLine}
+          bgColor={bgColor}
+          bgImage={bgImage}
         />
       )}
       {slide.split && (
         <SplitSlide
           title={slide.title}
           subTitle={slide.subTitle}
-          textArray={slide.text}
-          imageObject={slide.image}
+          text={slide.text}
+          image={slide.image}
+          h1Class={h1Class}
+          headerClass={headerClass}
+          leftBoxClass={leftBoxClass}
+          rightBoxClass={rightBoxClass}
+          imgBoxClass={imgBoxClass}
         />
       )}
       {slide.centered && slide.text && (
         <CenteredSlide
           title={slide.title}
           subTitle={slide.subTitle}
-          textArray={slide.text}
+          text={slide.text}
+          contentBoxClass={contentBoxClass}
+          contentTextClass={contentTextClass}
+          imageArrayBoxClass={imageArrayBoxClass}
         />
       )}
       {slide.centered && slide.images && (
         <CenteredSlide
           title={slide.title}
           subTitle={slide.subTitle}
-          imagesArray={slide.images}
+          images={slide.images}
         />
       )}
       {slide.splitEmbed && (
         <SplitSlideEmbed
           title={slide.title}
-          subTitle={slide.subTitle}
-          textArray={slide.text}
+          subTitle={slide?.subTitle}
+          textArray={slide?.text}
           embed={slide.embed}
         />
       )}
     </div>
   );
 }
-
-Layout.propTypes = {
-  slide: PropTypes.object.isRequired,
-  bgColor: PropTypes.string,
-  color: PropTypes.string
-};
 
 export default Layout;
