a66c52a91bc95b1aaec1da6c63eb753f7ac56e54
Nathan Michalewicz <nathan@mwicz.com>
2023-02-20 16:08:37 -0500

    FIX: img.onload in if statement

--- src/cacheImages.ts
+++ src/cacheImages.ts
@@ -1,22 +1,35 @@
-// import { ImgHTMLAttributes } from "react";
-
 /**
  * cacheImages takes in an array of images
  * to cache prior to using them.
  */
-async function cacheImages(props:{srcArray: Array<string>}) {
-  const promises = props.srcArray.map(src => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
+// async function cacheImages(srcArray: Array<string>) {
+//   const promises = srcArray.map((src: string) => {
+//     return new Promise((resolve, reject) => {
+//       const img = new Image();
 
-      img.src = src;
-      img.onload = resolve();
-      img.onerror = reject();
-    });
-  });
-  await Promise.all(promises);
-  const results = await Promise.allSettled(promises);
-  results.forEach(result => console.log(result.status));
-};
+//       img.src = src;
+//       // on img load, resolve promise.
+//       img.onload = resolve(src);
+//       // on error, reject promise.
+//       img.onerror = reject();
+//     });
+//   });
+//   await Promise.all(promises);
+//   const results = await Promise.allSettled(promises);
+//   results.forEach(result => console.log(result.status));
+// };
+function cacheImages({ images }: { images: Array<string>; }): Promise<Array<unknown>> {
+  return Promise.all(
+    images.map((image: string) => {
+      return new Promise((resolve, reject) => {
+        const img: HTMLImageElement = new Image();
+        img.src = image;
+        if (img.onload) console.log('onload'); resolve(image);
+        if (img.onerror) console.log('onerror'); reject();
+      });
+    })
+  );
+}
 
 export default cacheImages;
+
